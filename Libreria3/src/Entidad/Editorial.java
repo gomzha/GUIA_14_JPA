2fe3691d40784c5ba7622bd047059c8eaf3e10b0
Gonzalo Sánchez <gonsanchez@santandertecnologia.com.ar>
2023-03-13 21:26:09 -0300

    COMMIT 13-03-23

---
+++ Libreria3/src/Entidad/Editorial.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidad;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author A308443
+ */
+@Entity
+public class Editorial implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String nombre;
+     private boolean alta;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (int) id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Editorial)) {
+            return false;
+        }
+        Editorial other = (Editorial) object;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Entidad.Editorial[ id=" + id + " ]";
+    }
+    
+}
---
+++ Libreria3/src/Entidad/Libro.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidad;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author A308443
+ */
+@Entity
+public class Libro implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long isbn;
+    private String titulo;
+    private int anio, ejemplares, ejemplaresPrestados, ejemplaresRestamtes;
+    private boolean alta;
+    private Autor autor;
+    private Editorial editorial;
+
+    public Long getId() {
+        return isbn;
+    }
+
+    public void setId(Long isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (isbn != null ? isbn.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Libro)) {
+            return false;
+        }
+        Libro other = (Libro) object;
+        if ((this.isbn == null && other.isbn != null) || (this.isbn != null && !this.isbn.equals(other.isbn))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Entidad.Libro[ isbn=" + isbn + " ]";
+    }
+    
+}
